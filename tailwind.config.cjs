755695623d0088d06667620d97b93d6f73df2587
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-10-11 20:16:06 +0100

    config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '95': '95vh'
+      },
+      width: {
+        '95': '95vw'
+      },
+    },
   },
   plugins: [],
 }
